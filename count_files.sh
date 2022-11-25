968944fda90c1844ca9fa2fa76b4ca5c13a19c08
englandwe <frigginacky@gmail.com>
2022-11-25 12:12:39 -0700

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -4,3 +4,5 @@ find . -type f | cut -d/ -f2 | sort | uniq -c
 #same, 2 levels
 find . -type f | cut -d/ -f2,3 | sort | uniq -c
 #etc, etc
+#I only care about files of a certain type (i.e. .tsv)
+find . -type f -iname "*.tsv" | cut -d/ -f2 | sort | uniq -c
